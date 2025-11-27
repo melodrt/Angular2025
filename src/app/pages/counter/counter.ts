5ffa03d1185cc980ec3af26da3ff81e004f3812d
meloduarte <devscustomers@gmail.com>
2025-11-27 12:19:57 -0500

    senales en angular

--- src/app/pages/counter/counter.ts
+++ src/app/pages/counter/counter.ts
@@ -1,13 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
     templateUrl: './counter.html',
+    styles: `
+    button {
+    padding: 5px;
+    margin: 5px 10px;
+    width: 75px;
+    }
+    `
 })
 export class Counter {
   counter = 10;
+  value = 1;
+  CounterSignal = signal(10);
 
-  increaseBy(value: number)
-{
+  increaseBy(value: number) {
     this.counter += value;
-}
+    this.CounterSignal.update((current) => current + value);
+  }
+
+  resetCounter(value: number) {
+    this.counter = 0;
+    this.CounterSignal.set(0);
+  }
 }
